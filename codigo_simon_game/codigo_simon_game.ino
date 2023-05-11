d327a401f08636a1185bf1782170220a45f301dc
Thomaz-Klifson <110629744+Thomaz-Klifson@users.noreply.github.com>
2023-05-10 21:36:16 -0300

    Adicionando o código do Simon Game e piscaLed finalizados e com comentários

--- codigo_simon_game/codigo_simon_game.ino
+++ codigo_simon_game/codigo_simon_game.ino
@@ -14,39 +14,48 @@ int indexImput = 0;
 void setup() {
   Serial.begin(115200);
 
+  // Configurar LEDs como saída e desligá-los
   for (int i = 0; i < numLeds; i++) {
     pinMode(pinosLeds[i], OUTPUT);
     digitalWrite(pinosLeds[i], LOW);
   }
+  // Configurar botões como entrada com resistor interno pull-up
   for (int i = 0; i < numBotoes + 1; i++) {
     pinMode(pinosBotoes[i], INPUT_PULLUP);
   }
 
+  // Inicializar a semente aleatória com uma leitura analógica
   randomSeed(analogRead(0));
 }
 
 void loop() {
+  // Verificar se o botão de reset foi pressionado
   if (digitalRead(botaoReset) == LOW) {
     delay(1000);
     comprimentoSequencia = 0;
     comecarNovoRound();
   }
 
+  // Verificar se algum botão foi pressionado
   for (int i = 0; i < numBotoes; i++) {
     if (digitalRead(pinosBotoes[i]) == LOW) {
       delay(20); // Debounce
       if (digitalRead(pinosBotoes[i]) == LOW) {
+        // Acender LED correspondente ao botão pressionado
         digitalWrite(pinosLeds[i], HIGH);
         delay(300);
         digitalWrite(pinosLeds[i], LOW);
 
+        // Verificar se o botão pressionado corresponde à sequência atual
         if (i == sequencia[indexImput]) {
           indexImput++;
+          // Verificar se a sequência foi completada
           if (indexImput == comprimentoSequencia) {
             delay(1000);
             comecarNovoRound();
           }
         } else {
+          // Sequência incorreta, fim de jogo
           fimDeJogo();
         }
       }
@@ -54,13 +63,16 @@ void loop() {
   }
 }
 
+// Iniciar um novo round
 void comecarNovoRound() {
   indexImput = 0;
   comprimentoSequencia++;
 
+  // Verificar se o jogador venceu
   if (comprimentoSequencia > 20) {
     vitoria();
   } else {
+    // Mostrar a sequência atual
     for (int i = 0; i < comprimentoSequencia; i++) {
       if (i == comprimentoSequencia - 1) {
         sequencia[i] = random(0, numBotoes);
@@ -73,7 +85,9 @@ void comecarNovoRound() {
   }
 }
 
+// Fim de jogo, mostrar animação de derrota e reiniciar o jogo
 void fimDeJogo() {
+  // Piscar todos os LEDs três vezes
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < numLeds; j++) {
       digitalWrite(pinosLeds[j], HIGH);
@@ -84,11 +98,14 @@ void fimDeJogo() {
     }
     delay(250);
   }
+  // Reiniciar o jogo
   comprimentoSequencia = 0;
   delay(1000);
 }
 
+// Função para mostrar animação de vitória e reiniciar o jogo
 void vitoria() {
+  // Piscar todos os LEDs 10 vezes rapidamente
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < numLeds; j++) {
       digitalWrite(pinosLeds[j], HIGH);
@@ -96,6 +113,8 @@ void vitoria() {
       digitalWrite(pinosLeds[j], LOW);
     }
   }
+  // Reiniciar o jogo
   comprimentoSequencia = 0;
   delay(1000);
 }
+
